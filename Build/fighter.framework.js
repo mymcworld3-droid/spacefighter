08e6d0a5b8406ffb3647a10774ca87f548c95bdc
mymcworld3-droid <mymcworld3@gmail.com>
2025-10-25 17:35:38 +0800

    Initial Unity WebGL build with Git LFS support

---
+++ Build/fighter.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:665bd0d9ab2dd2ec31e6c138e555e8619b4e1e3584288c7f2148e10b5aa7bcb2
+size 470799
---
+++ Build/fighter.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d7ed0660c0ebd6f74cb1c50e9d6db6443a2f152e75670ed92df6d114457d975
+size 25250
